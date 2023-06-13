f05a8eda676099d2b4ac498a77162b2123a31e39
ahmed hamdy <ahamdy@lavad.app>
2023-06-13 22:54:15 +0300

    initial commit

---
+++ CardPaymentUI/CardPaymentUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardPaymentUIApp.swift
+//  CardPaymentUI
+//
+//  Created by Ahmed Hamdy on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardPaymentUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

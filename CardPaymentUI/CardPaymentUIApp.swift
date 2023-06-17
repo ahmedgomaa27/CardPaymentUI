4573ccd6069989731437df200b7e3be757e18900
ahmed hamdy <ahamdy@lavad.app>
2023-06-17 19:01:56 +0300

    handle if user clicks in the screen, close the keyboard

--- CardPaymentUI/CardPaymentUIApp.swift
+++ CardPaymentUI/CardPaymentUIApp.swift
@@ -11,7 +11,27 @@ import SwiftUI
 struct CardPaymentUIApp: App {
     var body: some Scene {
         WindowGroup {
+            //10 is just for testing, amount should be passed from the screen will use card payment screen
             CardPayment(viewModel: CardPaymentViewModel(amount: 10))
+                .onAppear(perform: UIApplication.shared.addTapGestureRecognizer)
         }
+    }
+}
+
+
+extension UIApplication {
+    func addTapGestureRecognizer() {
+        guard let window = windows.first else { return }
+        let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+        tapGesture.requiresExclusiveTouchType = false
+        tapGesture.cancelsTouchesInView = false
+        tapGesture.delegate = self
+        window.addGestureRecognizer(tapGesture)
+    }
+}
+
+extension UIApplication: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
